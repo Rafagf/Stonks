9a1dfa231499f049f5cb818fc1de98e34dc0c025
Rafa <rafael.g@memrise.com>
2021-07-24 19:54:53 +0200

    adding api search item response model

--- shared/src/commonMain/kotlin/com/rafag/stonks/SearchApi.kt
+++ shared/src/commonMain/kotlin/com/rafag/stonks/SearchApi.kt
@@ -1,7 +1,6 @@
 package com.rafag.stonks
 
 import com.rafag.stonks.Method.*
-import io.ktor.http.HttpMethod
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -15,39 +14,14 @@ class SearchApi {
 
 @Serializable
 data class ApiSearchResponse(
-    @SerialName("c") val current: Double,
-    @SerialName("h") val high: Double,
-    @SerialName("l") val low: Double,
-    @SerialName("o") val open: Double,
-    @SerialName("pc") val previousClose: Double,
+    @SerialName("result") val result: List<ApiSearchItemResponse>,
 )
 
-{
-    "count": 4,
-    "result": [
-    {
-        "description": "APPLE INC",
-        "displaySymbol": "AAPL",
-        "symbol": "AAPL",
-        "type": "Common Stock"
-    },
-    {
-        "description": "APPLE INC",
-        "displaySymbol": "AAPL.SW",
-        "symbol": "AAPL.SW",
-        "type": "Common Stock"
-    },
-    {
-        "description": "APPLE INC",
-        "displaySymbol": "APC.BE",
-        "symbol": "APC.BE",
-        "type": "Common Stock"
-    },
-    {
-        "description": "APPLE INC",
-        "displaySymbol": "APC.DE",
-        "symbol": "APC.DE",
-        "type": "Common Stock"
-    }
-    ]
-}
+@Serializable
+data class ApiSearchItemResponse(
+    @SerialName("description") val description: String,
+    @SerialName("displaySymbol") val displaySymbol: String,
+    @SerialName("symbol") val symbol: String,
+    @SerialName("type") val type: String,
+
+    )
