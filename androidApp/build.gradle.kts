f02cdb128fb09617007ff6c33db491febcf501b9
Rafa <rafael.g@memrise.com>
2021-09-18 13:37:58 +0200

    removing contraint layout dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${properties["version.kotlinx.coroutines"]}")
     implementation("androidx.compose.compiler:compiler:1.1.0-alpha04")
     implementation("androidx.compose.ui:ui:1.0.2")
