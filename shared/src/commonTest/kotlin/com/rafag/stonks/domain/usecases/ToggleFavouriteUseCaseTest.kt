7b106cf646ebdb24680192b2f17494ab25f203ff
Rafa <rafael.g@memrise.com>
2021-10-11 11:15:42 +0200

    adding toggle favourite use case tests

---
+++ shared/src/commonTest/kotlin/com/rafag/stonks/domain/usecases/ToggleFavouriteUseCaseTest.kt
@@ -0,0 +1,29 @@
+package com.rafag.stonks.domain.usecases
+
+import com.rafag.stonks.domain.repositories.FavouritesRepository
+import com.rafag.stonks.mock
+import com.rafag.stonks.runBlocking
+import com.rafag.stonks.verify
+import kotlin.test.Test
+
+private const val A_SYMBOL = "GOOG"
+
+class ToggleFavouriteUseCaseTest {
+
+    private val favouritesRepository = mock<FavouritesRepository>()
+    private val useCase = ToggleFavouriteUseCase(favouritesRepository)
+
+    @Test
+    fun `when saved then propagate to repository`() = runBlocking {
+        useCase.saved(A_SYMBOL)
+
+        verify(favouritesRepository).save(A_SYMBOL)
+    }
+
+    @Test
+    fun `when unsaved then propagate to repository`() = runBlocking {
+        useCase.unsaved(A_SYMBOL)
+
+        verify(favouritesRepository).unsave(A_SYMBOL)
+    }
+}
